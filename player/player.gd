c06cb5b4fac205274447bd7e0dfef2aa3a3574b3
Ricky Tham <rickytham123@gmail.com>
2021-10-23 19:47:43 -0700

    Add the player movement to tv with right click

--- player/player.gd
+++ player/player.gd
@@ -1,32 +1,42 @@
-extends KinematicBody2D
+extends RigidBody2D
 
-const WALK_FORCE = 600
-const WALK_MAX_SPEED = 200
-const STOP_FORCE = 1300
-const JUMP_SPEED = 200
+export(NodePath) var toFollow
+export var followSpeed: float
+var objToFollow : CollisionObject2D
 
-var velocity = Vector2()
 
-onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+# Declare member variables here. Examples:
+var moveVector : Vector2
+var move := false
 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("move_to_object"):
+		move = true
+		
+	if event.is_action_released("move_to_object"):
+		move = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	# Horizontal movement code. First, get the player's input.
-	var walk = WALK_FORCE * (Input.get_action_strength("move_right") - Input.get_action_strength("move_left"))
-	# Slow down the player if they're not trying to move.
-	if abs(walk) < WALK_FORCE * 0.2:
-		# The velocity, slowed down a bit, and then reassigned.
-		velocity.x = move_toward(velocity.x, 0, STOP_FORCE * delta)
+	if objToFollow and move:
+		moveVector = objToFollow.position - position
+		moveVector *= followSpeed
+		print(moveVector)
+		set_applied_force(moveVector)
 	else:
-		velocity.x += walk * delta
-	# Clamp to the maximum horizontal movement speed.
-	velocity.x = clamp(velocity.x, -WALK_MAX_SPEED, WALK_MAX_SPEED)
-
-	# Vertical movement code. Apply gravity.
-	velocity.y += gravity * delta
-
-	# Move based on the velocity and snap to the ground.
-	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
-
-	# Check for jumping. is_on_floor() must be called after movement code.
-	if is_on_floor() and Input.is_action_just_pressed("jump"):
-		velocity.y = -JUMP_SPEED
+		set_applied_force(Vector2())
+	var direction = sign(get_global_mouse_position().x - $Sprite.global_position.x)
+	if (rotation_degrees >= 130 and rotation_degrees <= 220) or (rotation_degrees <= -130 and rotation_degrees >= -220):
+		direction = direction * -1
+		
+	if direction < 0:
+		$Sprite.set_flip_h(false)
+		# $CollisionPolygon2D.scale.x = 1.804
+	else:
+		$Sprite.set_flip_h(true)
+		# $CollisionPolygon2D.scale.x = -1.804
--- television/television.gd
+++ television/television.gd
@@ -5,6 +5,11 @@ extends RigidBody2D
 # var a = 2
 # var b = "text"
 
+var picked = true
+
+func _physics_process(delta):
+	if picked == true:
+		self.position = get_node("../player/Position2D").global_position
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,7 +17,7 @@ func _ready():
 	set_friction(0.2)
 
 func launch(force : Vector2) -> void:
-	
+	picked = false
 	apply_impulse(Vector2.ZERO, force * 3)
 	
 
