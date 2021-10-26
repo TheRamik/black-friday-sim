72f7ea5cd3fd41ad33895fe70c93ebbdd802013e
Efren Aguilar <efrenaguilar95@hotmail.com>
2021-10-26 08:47:01 -0700

    Add debug mode

--- PlayerTV/PlayerTVv2.gd
+++ PlayerTV/PlayerTVv2.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 export(NodePath) var cooldownGUINode
 
+export var debugModeEnabledInGame = false
+var debugModeOn = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Replace with function body.
@@ -17,6 +20,9 @@ func _process(delta):
 		_enableVectorLauncher()
 	else:
 		$television/CanvasLayer/VectorCreator.visible = false
+		
+	if debugModeOn:
+		$television.position = get_global_mouse_position()
 
 func _enableVectorLauncher()->void:
 	if $television.colTimerStopped:
@@ -29,3 +35,7 @@ func _physics_process(delta):
 	if $television.picked:
 		$player.add_force(Vector2(), Vector2())
 		$player.set_applied_force(Vector2())
+		
+func _input(event):
+	if debugModeEnabledInGame and event.is_action_pressed("debug"):
+		debugModeOn = !debugModeOn
--- UI/GUI.gd
+++ UI/GUI.gd
@@ -1,17 +1,8 @@
 extends CanvasLayer
 
-
 func _input(event):
 	if event.is_action_pressed("pause"):
 		if not get_node("PauseMenu").visible:
 			get_node("PauseMenu").show()
 		else:
 			get_node("PauseMenu").hide()
-			
-
-	
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
